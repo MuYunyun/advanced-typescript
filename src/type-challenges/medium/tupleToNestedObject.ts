778c73686a124ebdd14c92978b937ac5b57ab715
MuYunyun <328375795@qq.com>
2021-09-13 19:01:21 +0800

    feat: tupleToNestedObject

---
+++ src/type-challenges/medium/tupleToNestedObject.ts
@@ -0,0 +1,43 @@
+import { Equal, Expect, Alike, NotAny } from '../../..'
+
+/*
+  3188 - Tuple to Nested Object
+  -------
+  by jiangshan (@jiangshanmeta) #medium #object
+
+  ### Question
+
+  Given a tuple type ```T``` that only contains string type, and a type ```U```, build an object recursively.
+
+  ```typescript
+  type a = TupleToNestedObject<['a'], string> // {a: string}
+  type b = TupleToNestedObject<['a', 'b'], number> // {a: {b: number}}
+  type c = TupleToNestedObject<[], boolean> // boolean. if the tuple is empty, just return the U type
+  ```
+
+  > View on GitHub: https://tsch.js.org/3188
+*/
+
+
+// type aaa = ['1']['length'] extends 1 ? true : false
+
+/* _____________ Your Code Here _____________ */
+type Shift<T extends any[]> = T extends [infer _, ...infer O] ? O : never
+
+type TupleToNestedObject<T extends any[], U> = T['length']
+// TupleToNestedObject<Shift<T>>
+
+/* _____________ Test Cases _____________ */
+type cases = [
+  Expect<Equal<TupleToNestedObject<['a'], string>, { a: string }>>,
+  Expect<Equal<TupleToNestedObject<['a', 'b'], number>, { a: { b: number } }>>,
+  Expect<Equal<TupleToNestedObject<['a', 'b', 'c'], boolean>, { a: { b: { c: boolean } } }>>,
+  Expect<Equal<TupleToNestedObject<[], boolean>, boolean>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/3188/answer
+  > View solutions: https://tsch.js.org/3188/solutions
+  > More Challenges: https://tsch.js.org
+*/
